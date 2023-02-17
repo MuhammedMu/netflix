8981c17844b0d1508f62d8121f64bc2bd101a398
MuhammedMu <120379746+MuhammedMu@users.noreply.github.com>
2023-02-17 22:17:47 +0300

    Hiding env file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './Components/Banner/Banner';
+import Nav from './Components/Nav/Nav';
+import Rows from './Components/Rows/Rows';
+import requests from './requests';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+
+      <Rows
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLarge={true}
+      />
+      <Rows
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        isLarge={true}
+      />
+
+      <Rows
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRatedMovies}
+        isLarge={false}
+      />
+      <Rows
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        isLarge={false}
+      />
+      <Rows
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        isLarge={false}
+      />
+      <Rows
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        isLarge={false}
+      />
+      <Rows
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        isLarge={false}
+      />
+      <Rows
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        isLarge={false}
+      />
     </div>
   );
 }
